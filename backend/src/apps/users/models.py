18b77dd64f1356577674ac78c496fd8eb3acab26
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-17 10:17:42 +0300

    [ADD] users models * Role * User

---
+++ backend/src/apps/users/models.py
@@ -0,0 +1,24 @@
+from tortoise import models
+from tortoise import fields
+
+from fastapi_users.db import TortoiseBaseUserModel
+
+from apps.common.models import AbstractIntIDModel
+from apps.common.mixins import TimestampMixin
+
+
+class Role(AbstractIntIDModel):
+    name = fields.CharField(max_length=255, unique=True)
+    description = fields.TextField(null=True)
+    permissions = fields.JSONField(default=dict)
+
+    class Meta:
+        table = "users_role"
+
+
+class User(TimestampMixin, TortoiseBaseUserModel, AbstractIntIDModel):
+    username = fields.CharField(max_length=255, unique=True)
+    role = fields.ForeignKeyField('models.Role', on_delete=fields.SET_NULL, null=True)
+
+    class Meta:
+        table = "users_user"

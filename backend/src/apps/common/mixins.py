acb4956a0a00176bd12104935c7381a147d85599
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-17 10:06:29 +0300

    [ADD] common mixins

---
+++ backend/src/apps/common/mixins.py
@@ -0,0 +1,7 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class TimestampMixin:
+    created_at = fields.DatetimeField(null=True, auto_now_add=True)
+    modified_at = fields.DatetimeField(null=True, auto_now=True)

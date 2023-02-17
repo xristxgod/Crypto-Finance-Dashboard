7f67dd2effa62afc9a7c782045bb5a377ca6a121
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-17 13:18:15 +0300

    [FIX]

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -19,7 +19,7 @@ class Role(AbstractIntIDModel):
 
 class User(TimestampMixin, TortoiseBaseUserModel):
     username = fields.CharField(max_length=255, unique=True)
-    email = fields.CharField(index=True, unique=True, null=False, max_length=255, validators=[EmailValidator()])
+    email = fields.CharField(index=True, unique=True, null=True, max_length=255, validators=[EmailValidator()])
     phone_number = fields.CharField(max_length=20, unique=True, null=True, validators=[RUPhoneValidator()])
     role = fields.ForeignKeyField('crypto_dashboard.Role', on_delete=fields.SET_NULL, null=True, related_name='users')
 

ed808133efc0459c710235ab36abfbe8194bc96b
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-17 11:52:03 +0300

    [ADD] auth config

---
+++ backend/src/apps/auth/config.py
@@ -0,0 +1,36 @@
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import CookieAuthentication, JWTAuthentication
+
+from config import settings
+from . import schemas
+from . import managers
+
+
+cookie_authentication = CookieAuthentication(
+    secret=settings.SECRET_KEY_AUTH,
+    cookie_secure=False,
+    cookie_httponly=False,
+)
+
+jwt_authentication = JWTAuthentication(
+    secret=settings.SECRET_KEY_AUTH,
+    lifetime_seconds=3600,
+)
+
+
+fastapi_cookies = FastAPIUsers(
+    managers.get_user_manager,
+    (
+        # Cookie
+        cookie_authentication,
+        # JWT
+        jwt_authentication,
+    ),
+    schemas.BodyUser,
+    schemas.BodyUserCreate,
+    schemas.BodyUserUpdate,
+    schemas.UserDB,
+)
+
+get_current_active_user = fastapi_cookies.current_user(active=True)
+get_current_superuser = fastapi_cookies.current_user(superuser=True)

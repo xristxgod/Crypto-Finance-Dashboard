86a010d78ff37998d2254c884914c0c5232e8dd9
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-17 09:35:52 +0300

    [ADD] common settings

---
+++ backend/src/settings/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .common import *
+
+
+__all__ = (
+    'Settings',
+)
+
+
+class Settings:
+    ROOT_DIR = ROOT_DIR
+    DATABASE_CONFIG: dict = DATABASE_CONFIG

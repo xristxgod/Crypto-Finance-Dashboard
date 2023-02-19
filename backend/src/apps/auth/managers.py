bac1f968171296b0c302096b7454eab8afca1e64
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-19 14:50:53 +0300

    [ADD] custom auth by username

--- backend/src/apps/auth/managers.py
+++ backend/src/apps/auth/managers.py
@@ -2,7 +2,9 @@ from typing import Optional
 
 from fastapi.params import Depends
 from fastapi.requests import Request
-from fastapi_users.manager import BaseUserManager
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_users.manager import BaseUserManager, UserNotExists
+from fastapi_users import models, password
 from fastapi_users.db import TortoiseUserDatabase
 
 from config import settings
@@ -28,6 +30,37 @@ class UserManager(BaseUserManager[BodyUserCreate, UserDB]):
 
     async def on_after_request_verify(self, user: UserDB, token: str, request: Optional[Request] = None):
         print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+    async def get_by_username(self, user_username: str):
+        user = await self.user_db.get_by_username(user_username)
+
+        if user is None:
+            raise UserNotExists()
+
+        return user
+
+    async def authenticate(
+        self, credentials: OAuth2PasswordRequestForm
+    ) -> Optional[models.UD]:
+        try:
+            user = await self.get_by_username(credentials.username)
+        except UserNotExists:
+            # Run the hasher to mitigate timing attack
+            # Inspired from Django: https://code.djangoproject.com/ticket/20760
+            password.get_password_hash(credentials.password)
+            return None
+
+        verified, updated_password_hash = password.verify_and_update_password(
+            credentials.password, user.hashed_password
+        )
+        if not verified:
+            return None
+        # Update password hash to a more robust one if needed
+        if updated_password_hash is not None:
+            user.hashed_password = updated_password_hash
+            await self.user_db.update(user)
+
+        return user
 
 
 def get_user_manager(user_db: TortoiseUserDatabase = Depends(get_user_db)):
--- backend/src/apps/auth/utils.py
+++ backend/src/apps/auth/utils.py
@@ -1,4 +1,8 @@
+from typing import cast
+
+from tortoise.contrib.pydantic.base import PydanticModel
 from fastapi_users.db import TortoiseUserDatabase
+from fastapi_users import models
 
 from apps.users.schemas import UserDB
 from apps.users.models import User
@@ -8,5 +12,25 @@ __all__ = (
 )
 
 
+class CustomTortoiseUserDatabase(TortoiseUserDatabase):
+
+    async def get_by_username(self, username: str):
+        query = self.model.filter(username__iexact=username).first()
+
+        if self.oauth_account_model is not None:
+            query = query.prefetch_related("oauth_accounts")
+
+        user = await query
+
+        if user is None:
+            return None
+
+        pydantic_user = await cast(PydanticModel, self.user_db_model).from_tortoise_orm(
+            user
+        )
+
+        return cast(models.UD, pydantic_user)
+
+
 def get_user_db():
-    yield TortoiseUserDatabase(UserDB, User)
+    yield CustomTortoiseUserDatabase(UserDB, User)

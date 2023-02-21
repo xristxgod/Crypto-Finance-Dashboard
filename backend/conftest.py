3209666699158834033568fc66199c1d6e460cad
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-21 16:16:51 +0300

    [ADD] conftest.py [ADD] pyproject.toml

---
+++ backend/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from httpx import AsyncClient
+
+from src.main import app
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    import asyncio
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+async def client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def initialize_tests():
+    from tortoise import Tortoise
+    from src.core import database
+    from src.config import settings
+
+    await Tortoise.init(config=settings.TEST_DATABASE_CONFIG)
+    await Tortoise.generate_schemas()
+
+    yield
+
+    await Tortoise.close_connections()
+    await database.drop()

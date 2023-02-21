0756a5db33c0fb78ab45bf3e092c2b57d7db16d2
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-21 16:08:33 +0300

    [FIX]

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -12,7 +12,9 @@ class User(TimestampMixin, TortoiseBaseUserModel):
     email = fields.CharField(index=True, unique=True, null=True, max_length=255, validators=[EmailValidator()])
     phone_number = fields.CharField(max_length=20, unique=True, null=True, validators=[RUPhoneValidator()])
 
-    telegram = fields.ReverseRelation['Telegram']
+    telegram = fields.ReverseRelation['default.Telegram']
+    telegram_referral_link = fields.ReverseRelation['default.TelegramReferralLink']
+    accounts = fields.ReverseRelation['default.Account']
 
     class Meta:
         table = "users_user"

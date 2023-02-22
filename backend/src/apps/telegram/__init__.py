c7205c0413ee7c5794e418a40a5dbc3097a580b5
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-22 12:06:15 +0300

    [FIX] [UPD] telegram structure

---
+++ backend/src/apps/telegram/__init__.py
@@ -0,0 +1,7 @@
+from apps.telegram.models import Telegram, TelegramReferralLink
+from apps.telegram.schemas import TelegramDB, BodyTelegram, BodyTelegramReferralLink
+
+__all__ = (
+    'Telegram', 'TelegramReferralLink',
+    'TelegramDB', 'BodyTelegram', 'BodyTelegramReferralLink'
+)

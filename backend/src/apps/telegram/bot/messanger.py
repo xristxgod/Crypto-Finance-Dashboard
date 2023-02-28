61f96a3b7f4eb63b2100315fe4b452ad919f40de
Mamedov Murad <mamedov_99b@mail.ru>
2023-02-28 13:08:51 +0300

    [ADD] make text

--- backend/src/apps/telegram/bot/messanger.py
+++ backend/src/apps/telegram/bot/messanger.py
@@ -1,10 +1,13 @@
+import re
 import json
 from typing import Any
 
+import emoji
 from aiogram import types
 from tortoise import transactions
 from fastapi_cache.decorator import cache
 
+from config import settings
 from apps.telegram.models import Message
 from apps.telegram.bot.services import BaseUser
 from apps.telegram.config import TELEGRAM_CONFIG_DIR
@@ -17,12 +20,13 @@ EXPIRE_TIME = 60
 
 
 class messanger:
+    args_pattern = re.compile(r'(?P<args>{{[A-Z]+}})')
 
     tags = (
         'referral_code_not_found',
         'user_not_found',
         'start',
-        'success_registration'
+        'success_registration',
         'menu',
     )
 
@@ -31,13 +35,13 @@ class messanger:
         import aiofiles
         async with transactions.in_transaction('default'):
             async with aiofiles.open(TELEGRAM_CONFIG_DIR / 'default_en_messages.json') as file:
-                default_messages = json.dumps(await file.read())
+                default_messages = json.loads(await file.read())
 
             update_message = []
             for message in messages:
                 valid_default_message = list(filter(lambda x: x['tag'] == message.tag, default_messages))
                 if valid_default_message:
-                    setattr(message, 'message_ENG', valid_default_message[0].get('message_ENG'))
+                    setattr(message, 'message_ENG', json.dumps(valid_default_message[0].get('message_ENG')))
                     update_message.append(message)
 
             await Message.bulk_update(update_message, fields=['message_ENG'])
@@ -54,8 +58,15 @@ class messanger:
                     for tag in set(cls.tags).difference(database_tags)
                 ])
                 if set_default_message:
-                    await cls.__set_default_message(messages.objects)
+                    await cls.__set_default_message(messages)
 
+    @classmethod
+    async def _make_text(cls, text: str):
+        for raw_arg in re.findall(cls.args_pattern, text):
+            arg = raw_arg.replace('{{', '').replace('}}', '')
+            text = text.replace(raw_arg, str(getattr(settings, arg)))
+
+        return emoji.emojize(text, language='alias')
 
     @classmethod
     async def _make_message(cls, db_message: dict[str, Any], user: BaseUser, message: types.Message):
@@ -63,12 +74,14 @@ class messanger:
 
         message_conf = {
             'chat_id': user.chat_id,
-            'text': db_message.get('message', 'Error')
+            'text': await cls._make_text(db_message.get('message', 'Error'))
         }
 
         if db_message.get('inline_button'):
             keyboard = types.InlineKeyboardMarkup()
             for button in db_message['inline_button']:
+                if button.get('text'):
+                    button['text'] = await cls._make_text(button['text'])
                 keyboard.add(types.InlineKeyboardButton(**button))
             message_conf['reply_markup'] = keyboard
 
